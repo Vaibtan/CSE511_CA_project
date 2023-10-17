972afc18f2a89323b9b2f280a2b2b5e7d498592b
Animesh Pareek <108825332+Mr-AniP@users.noreply.github.com>
2023-10-17 10:51:56 +0530

    Update simulator.cpp

--- simulator.cpp
+++ simulator.cpp
@@ -1,10 +1,20 @@
 #include "cpu.hpp"
 // include logging header
+#include "LogFile.cpp"
 
-void start(RISCV_cpu *cpu){    
-    CPU_reset(cpu);
-    cpu->__pipe->cycle=0;
-    while(cpu->pc!=INST_END){ // INST_END Needs to programed /Defined somewhere
+void start(RISCV_cpu *cpu,u32 INST_END){
+    FILE* fp = fopen("logfile.log","a");
+    if(fp==NULL){
+        fprintf(stderr,"Error in opening logfile\n");
+        exit(1);
+    }
+    fprintf(fp, "****************************************************************************\n");
+    fprintf(fp, "*****************************RISCV SIMULATOR*******************************\n");
+    time_t t = time(NULL);
+    struct tm *tm = localtime(&t);
+    fprintf(fp, "Date and Time: %s\n", asctime(tm) );
+    fprintf(fp, "\n");
+    while(cpu->pc<INST_END){
         cpu_fetch(cpu);
         cpu_decode(cpu);
         cpu_execute(cpu);
@@ -13,18 +23,38 @@ void start(RISCV_cpu *cpu){
         cpu_pc_update(cpu);
         // statechange(cpu->__pipe);
         statechange_withoutBYPASSING(cpu->__pipe);
-        // call savelog(cpu, pipe) 
+        // call savelog(cpu, pipe)
+        logValues(fp,cpu, cpu->__pipe);
         cpu->__pipe->cycle+=1;
     }
+    fclose(fp);
+}
+u32 load_Instrucctions_in_memory(RISCV_cpu* cpu,char* path){
+    FILE* fp = fopen(path,"rb");
+    if(fp==NULL){
+        fprintf(stderr,"Error in opening binary file\n");
+        exit(1);
+    }
+    u32  INST_END= 0;
+    u32 INST = 0;
+    while(fread(&INST,sizeof(u32),1,fp)){
+        cpu_st(cpu, INST_END, 32, INST);// to be checked
+        INST_END+=4;
+    }
+    fclose(fp);
+    return INST_END;
 }
 
-int main(){
-    RISCV_cpu* mycpu = CPU_init(); 
+int main(int argc, char* argv[]){
 
-    // Instruction loading needs to be done to memory 
-    // mechanism of instruction end for while loop needs to be defined
-    // logging needs to be configured in the start function
-    
-    start(mycpu);
+    RISCV_cpu* mycpu = CPU_init(); 
+    if(argc!=2){
+        fprintf(stderr,"No input to simulator: <path to bin file missing> command line arguments no-> %d\n",argc);
+        exit(1);
+    }
+    u32 INST_END = load_Instrucctions_in_memory(mycpu,argv[1]);
+    CPU_reset(mycpu);
+    mycpu->__pipe->cycle=0;
+    start(mycpu,INST_END);
     return 0;
 }
