d51184c656b4f0da28cd246ead321bab13a0dd09
Ruchi Kumari <122676573+Ruchi-here@users.noreply.github.com>
2023-11-12 11:54:25 +0530

    Update LogFile.cpp

--- Simulator/src/LogFile.cpp
+++ Simulator/src/LogFile.cpp
@@ -15,13 +15,30 @@ void RegValues(FILE* file, RISCV_cpu* cpu){
     }
 }
 
-void logValues(FILE* file, RISCV_cpu *cpu, pipeline* pipe) {
-    fprintf(file, "Cycle %d:\n", pipe->cycle);
+void pipelineValues(FILE* file, RISCV_cpu *cpu, pipeline* pipe) {
+    
     fprintf(file, "\n");
-    fprintf(file, "CPU Context:\n");
-    RegValues(file, cpu);
-    // fprintf(file, "Memory state:\n");
-    // memstate(file, cpu);
+    fprintf(file, "Pipeline State:\n");
+
+    fprintf(file, "IF: Instr 0x%x\n", pipe->fetch->instr);
+    fprintf(file, "ID: Instr 0x%x\n", pipe->decode->instr);
+    fprintf(file, "EX: Instr 0x%x\n", pipe->execute->instr);
+    fprintf(file, "MEM: Instr 0x%x\n", pipe->memory->instr);
+    fprintf(file, "WB: Instr 0x%x\n", pipe->writeback->instr);
+    
     fprintf(file, "\n");
     fprintf(file, "****************************************************************************\n");
+}
+
+
+void logValues(FILE* file, RISCV_cpu *cpu, pipeline* pipe) {
+
+    fprintf(file, "Cycle %d:\n", pipe->cycle);
+    fprintf(file, "\n");
+
+    fprintf(file, "CPU Context:\n");
+
+    RegValues(file, cpu);
+
+    pipelineValues(file, cpu, pipe);
 }
