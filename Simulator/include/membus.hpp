4a5df1938d8140f996262d1fc10059fd2ba91c44
Animesh Pareek <108825332+Mr-AniP@users.noreply.github.com>
2023-11-19 17:44:02 +0530

    Update membus.hpp

--- Simulator/include/membus.hpp
+++ Simulator/include/membus.hpp
@@ -1,6 +1,5 @@
 #ifndef MEMBUS_H
 #define MEMBUS_H
-#include "utils.hpp"
 #include "memory.hpp"
 
 struct DATA_MEM_BUS {
@@ -11,12 +10,20 @@ struct INSTR_MEM_BUS {
     struct RISC_INSTR_mem* instr_mem;
 };
 
+struct MEM_BUS {
+    struct DATA_MEM_BUS* data_bus;
+    struct INSTR_MEM_BUS* instr_bus;
+};
+
+INSTR_MEM_BUS* instr_bus_init(INSTR_mem* mem);
+DATA_MEM_BUS* data_bus_init(DATA_mem* mem);
+MEM_BUS* mem_bus_init(DATA_MEM_BUS* d_bus,INSTR_MEM_BUS* i_bus);
+
 u32 data_bus_ld(DATA_MEM_BUS* _bus, u32 addr, u32 size);
 void data_bus_st(DATA_MEM_BUS* _bus, u32 addr, u32 size, u32 value);
-DATA_MEM_BUS* data_bus_init(RISC_DATA_mem* data_mem);
+
 
 u32 instr_bus_ld(INSTR_MEM_BUS* _bus, u32 addr, u32 size);
 void instr_bus_st(INSTR_MEM_BUS* _bus, u32 addr, u32 size, u32 value);
-INSTR_MEM_BUS* instr_bus_init(RISC_INSTR_mem* instr_mem);
 
 #endif
