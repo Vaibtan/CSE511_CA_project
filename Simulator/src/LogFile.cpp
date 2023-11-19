59aa99fc6eb4154118d9a211dd74e72aa17fc5c3
Animesh Pareek <108825332+Mr-AniP@users.noreply.github.com>
2023-11-19 18:11:27 +0530

    Update LogFile.cpp

--- Simulator/src/LogFile.cpp
+++ Simulator/src/LogFile.cpp
@@ -2,13 +2,17 @@
 
 void memstate(FILE* file, RISCV_cpu* cpu){
     fprintf(file, "Memory location  :   Data\n");
-    REP(__i, 0, RISCV_MEM_SIZE){ fprintf(file, "0x%x  :  0x%x\n", __i, cpu->__bus->riscv_mem->rv32i_mem[__i]); }
+    for(int i=0;i<RISCV_MEM_SIZE/2;i++){
+        fprintf(file, "0x%x     :   0x%x\n", i, cpu->__bus->data_bus->data_mem->rv32i_data_mem[i]);
+    }
 }
 
 void RegValues(FILE* file, RISCV_cpu* cpu){
     fprintf(file, "PC: 0x%x\n", cpu->pc);
     fprintf(file, "The current value of registers:\n");
-    REP(__i, 0, REG_LEN){ fprintf(file, "Register %d: 0x%x\n", __i, cpu->x[__i]); }
+    for(int i=0;i<REG_LEN;i++){
+        fprintf(file, "Register %d: 0x%x\n", i, cpu->x[i]);
+    }
 }
 
 void logValues(FILE* file, RISCV_cpu *cpu, pipeline* pipe) {
