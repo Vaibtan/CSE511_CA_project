1b01c71bac52d55d0f4602e23a704860489715a7
Vaibtan <vaibhav21296@iiitd.ac.in>
2023-11-19 17:12:16 +0530

    incomplete BC

--- Simulator/src/membus.cpp
+++ Simulator/src/membus.cpp
@@ -1,15 +1,16 @@
 #include "membus.hpp"
 
-u32 mem_bus_ld(MEM_BUS* __bus, u32 addr, u32 size){
-    return mem_ld((__bus->riscv_mem), addr,size);
+u32 data_bus_ld(DATA_MEM_BUS* _bus, u32 addr, u32 size){
+    return d_mem_ld((_bus->data_mem), addr,size);
 }
 
-void mem_bus_st(MEM_BUS* __bus, u32 addr, u32 size, u32 value){
-    mem_st((__bus->riscv_mem), addr, size, value);
+void data_bus_st(DATA_MEM_BUS* _bus, u32 addr, u32 size, u32 value){
+    d_mem_st((_bus->data_mem), addr, size, value);
 }
 
-MEM_BUS* mem_bus_init(RISC_mem* mem){
-    MEM_BUS* __bus = (MEM_BUS*)malloc(sizeof(MEM_BUS));
-    __bus->riscv_mem = mem;
-    return __bus;
+DATA_MEM_BUS* data_bus_init(DATA_mem* mem){
+    DATA_MEM_BUS* _bus = (DATA_MEM_BUS*)malloc(sizeof(DATA_MEM_BUS));
+    _bus->data_mem = mem;
+    return _bus;
 }
+
--- Simulator/src/memory.cpp
+++ Simulator/src/memory.cpp
@@ -1,38 +1,38 @@
 #include "utils.hpp"
 #include "memory.hpp"
 
-u32 mem_ld_8(RISC_mem* mem, u32 addr){
-    return (u32) mem->rv32i_mem[addr - RISCV_MEM_BASE];
+u32 d_mem_ld_8(DATA_mem* mem, u32 addr){
+    return (u32) mem->rv32i_data_mem[addr - RISCV_MEM_BASE];
 }
-u32 mem_ld_16(RISC_mem* mem, u32 addr){
-    return (u32) mem->rv32i_mem[addr - RISCV_MEM_BASE]
-        |  (u32) mem->rv32i_mem[addr - RISCV_MEM_BASE + 1] << 8;
+u32 d_mem_ld_16(DATA_mem* mem, u32 addr){
+    return (u32) mem->rv32i_data_mem[addr - RISCV_MEM_BASE]
+        |  (u32) mem->rv32i_data_mem[addr - RISCV_MEM_BASE + 1] << 8;
 }
-u32 mem_ld_32(RISC_mem* mem, u32 addr){
-    return (u32) mem->rv32i_mem[addr - RISCV_MEM_BASE]
-        |  (u32) mem->rv32i_mem[addr - RISCV_MEM_BASE + 1] << 8
-        |  (u32) mem->rv32i_mem[addr - RISCV_MEM_BASE + 2] << 16 
-        |  (u32) mem->rv32i_mem[addr - RISCV_MEM_BASE + 3] << 24;
+u32 d_mem_ld_32(DATA_mem* mem, u32 addr){
+    return (u32) mem->rv32i_data_mem[addr - RISCV_MEM_BASE]
+        |  (u32) mem->rv32i_data_mem[addr - RISCV_MEM_BASE + 1] << 8
+        |  (u32) mem->rv32i_data_mem[addr - RISCV_MEM_BASE + 2] << 16 
+        |  (u32) mem->rv32i_data_mem[addr - RISCV_MEM_BASE + 3] << 24;
 }
 
-u32 mem_ld(RISC_mem* mem, u32 addr, u32 sz_) {
+u32 d_mem_ld(DATA_mem* mem, u32 addr, u32 sz_) {
     switch (sz_) {
-        case 8:  return mem_ld_8(mem, addr);  break;
-        case 16: return mem_ld_16(mem, addr); break;
-        case 32: return mem_ld_32(mem, addr); break;
+        case 8:  return d_mem_ld_8(mem, addr);  break;
+        case 16: return d_mem_ld_16(mem, addr); break;
+        case 32: return d_mem_ld_32(mem, addr); break;
         default: ;
     }
     return 1;
 }
 
 
-void mem_st_8(RISC_mem* mem, u64 addr, u64 value) {
-    mem->rv32i_mem[addr - RISCV_MEM_BASE] = (u8) (value & 0xff);
+void d_mem_st_8(DATA_mem* mem, u64 addr, u64 value) {
+    mem->rv32i_data_mem[addr - RISCV_MEM_BASE] = (u8) (value & 0xff);
 }
 
 void mem_st_16(RISC_mem* mem, u64 addr, u64 value) {
     mem->rv32i_mem[addr - RISCV_MEM_BASE] = (u8) (value & 0xff);
-    mem->rv32i_mem[addr - RISCV_MEM_BASE+1] = (u8) ((value >> 8) & 0xff);
+    mem->rv32i_mem[addr - RISCV_MEM_BASE + 1] = (u8) ((value >> 8) & 0xff);
 }
 
 void mem_st_32(RISC_mem* mem, u32 addr, u64 value) {
@@ -51,9 +51,9 @@ void mem_st(RISC_mem* mem, u32 addr, u32 sz_, u32 value) {
     }
 }
 
-RISC_mem *mem_init(){
-    RISC_mem *mem = (RISC_mem*)malloc(sizeof(RISC_mem));
-    if (mem==NULL) {
+DATA_mem *data_init(){
+    RISC_mem *mem = (RISC_mem*)malloc(sizeof(DATA_mem));
+    if (mem == NULL) {
         fprintf(stderr, "[-] ERROR-> mem_init : malloc failed\n");
         exit(1);
     }
