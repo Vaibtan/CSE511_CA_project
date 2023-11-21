ad9b166ee74f594e737e254c3ccf0bedef3c6086
Animesh Pareek <108825332+Mr-AniP@users.noreply.github.com>
2023-11-21 23:43:44 +0530

    Update cpu.hpp

--- Simulator/include/cpu.hpp
+++ Simulator/include/cpu.hpp
@@ -12,25 +12,24 @@
 struct RISCV_cpu{
     u32 x[REG_LEN];
     u32 mem_map_reg[MM_REG_LEN];
+    u32 vec[REG_LEN][vector_size];//vector array
     u32 pc;
     struct cache_bus* __bus;
     struct pipeline* __pipe;
     ALU* __alu;
     int memory_instr_counter;
     int register_instr_counter;
+    int noc_type_instr;
+    int simd_type_instr;
 };
 
 RISCV_cpu* CPU_init();
 void CPU_reset(RISCV_cpu *cpu);
-
-
 void cpu_fetch(RISCV_cpu *cpu,u32 new_pc,bool early_exit);
 void cpu_decode(RISCV_cpu *cpu);
 void cpu_execute(RISCV_cpu *cpu);
 void cpu_memory(RISCV_cpu *cpu);
 void cpu_writeback(RISCV_cpu*cpu);
 u32 cpu_pc_update(RISCV_cpu*cpu);
-//needed for initiall loading of instructions
-void cpu_st(RISCV_cpu* cpu, u32 addr, u32 size, u32 value);
 
 #endif
